aac6ebadcfbf4a488388644b6836f8c5e18b2bf9
diegoo.1795 <diegoo.1795@gmail.com>
2021-07-08 12:12:44 +0200

    BuildBean

---
+++ src/main/java/com/example/build/entities/Build.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.build.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import lombok.Data;
+import javax.validation.constraints.Pattern;
+
+/**
+ *
+ * @author diego
+ */
+
+
+@Entity
+@Data
+public class Build {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    
+    @NotEmpty 
+    @Pattern(regexp="^[A-Za-z]*$",message = "Invalid Input")
+    private String name;
+
+    @NotEmpty 
+    @Pattern(regexp="^[a-z\\d]+(?:\\/[a-z\\d]+)+$",message = "Invalid Input")
+    private String pathRepo;
+    
+    @NotEmpty 
+    @Pattern(regexp="\\d+(?:\\.\\d+)+",message = "Invalid Input")
+    private String version;
+}
